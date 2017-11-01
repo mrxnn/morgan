fe3c3158d95dc999d7064668fc078c042eaebb8e
mayura-ramanayaka <mayura.ramanayake@gmail.com>
2017-10-31 23:16:56 -0700

    Fixed the issue of Side Menu Animation being played at design time

--- Morgan/Animation/AnimationAttachedProperty.cs
+++ Morgan/Animation/AnimationAttachedProperty.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Media.Animation;
 
@@ -6,53 +7,57 @@ namespace Morgan
 {
     public class AnimationAttachedProperty
     {
-        #region AnimateInFromRightProperty
+        #region AnimateInFromLeftProperty
 
         /// <summary>
         /// The property that is used to animate the side menu when the application is first loaded
         /// </summary>
-        public static readonly DependencyProperty AnimateInFromRightProperty =
+        public static readonly DependencyProperty AnimateInFromLeftProperty =
             DependencyProperty.RegisterAttached(
-                "AnimateInFromRight", typeof(bool), typeof(AnimationAttachedProperty),
+                "AnimateInFromLeft", typeof(bool), typeof(AnimationAttachedProperty),
                 new PropertyMetadata(false,
-                    OnAnimateInFromRightPropertyChanged));
+                    OnAnimateInFromLeftPropertyChanged));
 
         /// <summary>
-        /// Callback that is fired whenever the <see cref="AnimateInFromRightProperty"/> is changed
+        /// Callback that is fired whenever the <see cref="AnimateInFromLeftProperty"/> is changed
         /// </summary>
         /// <param name="d">The WPF element that this property is attached to</param>
         /// <param name="e">Args describing the event</param>
-        private static void OnAnimateInFromRightPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void OnAnimateInFromLeftPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             // Make sure the element is valid
             if (!(d is FrameworkElement element))
+                return;
+
+            // Prevent the animation being played at design time
+            if (DesignerProperties.GetIsInDesignMode(element))
                 return;
 
             // If the value we set to is True...
             if ((bool)e.NewValue)
             {
                 // Attach an event handler to the loaded event to play the animation
-                element.Loaded += (ss, ee) => SlideInFromRight(element);
+                element.Loaded += (ss, ee) => SlideInFromLeft(element);
             }
         }
 
         // SETTER
-        public static void SetAnimateInFromRight(DependencyObject element, bool value)
-            => element.SetValue(AnimateInFromRightProperty, value);
+        public static void SetAnimateInFromLeft(DependencyObject element, bool value)
+            => element.SetValue(AnimateInFromLeftProperty, value);
 
         // GETTER
-        public static bool GetAnimateInFromRight(DependencyObject element)
-            => (bool)element.GetValue(AnimateInFromRightProperty);
+        public static bool GetAnimateInFromLeft(DependencyObject element)
+            => (bool)element.GetValue(AnimateInFromLeftProperty);
 
         #endregion
 
         #region Helper Methods
 
         /// <summary>
-        /// Plays an animation on any framework element, moving the element from right to left (to where the element is located by default)
+        /// Plays an animation on any framework element, moving the element from left to right (to where the element is located by default)
         /// </summary>
         /// <param name="element">The element to animate</param>
-        private static void SlideInFromRight(FrameworkElement element)
+        private static void SlideInFromLeft(FrameworkElement element)
         {
             // Create the storyboard
             var sb = new Storyboard();
