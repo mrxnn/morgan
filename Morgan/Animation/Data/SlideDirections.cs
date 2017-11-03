754808d06cf295e3d5d8aaa200579a726f778611
mayura-ramanayaka <mayura.ramanayake@gmail.com>
2017-11-02 19:18:50 -0700

    Added a basic animation system

---
+++ Morgan/Animation/Data/SlideDirections.cs
@@ -0,0 +1,18 @@
+﻿namespace Morgan
+{
+    public enum SlideInFrom
+    {
+        Left=0,
+        Top=1,
+        Right=2,
+        Bottom=4
+    }
+
+    public enum SlideOutTo
+    {
+        Left = 0,
+        Top = 1,
+        Right = 2,
+        Bottom = 4
+    }
+}
---
+++ Morgan/Animation/FrameworkElementAnimation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace Morgan
+{
+    /// <summary>
+    /// Extensions and a collection of helper methods to make it easier to make new animations!
+    /// </summary>
+    public static class FrameworkElementAnimation
+    {
+        /// <summary>
+        /// Adds a SLIDE IN animation to a storyboard
+        /// </summary>
+        /// <param name="storyboard">The storyboard to add the animation to</param>
+        /// <param name="offset">Offset to start animating from</param>
+        /// <param name="from">Slide direction</param>
+        /// <param name="duration">Time that it takes to complete the animation</param>
+        /// <param name="adjustLayout">Flag indicating if the layout adjustments should be visible or not</param>
+        /// <param name="decelerationRatio">Deceleration ratio</param>
+        /// <param name="easing">Easing Mode that is used for the easing function</param>
+        public static void AddSlideInAnimation(this Storyboard storyboard, double offset, SlideInFrom from, float duration = .8f, bool adjustLayout = true, float decelerationRatio = .9f, EasingMode easing = EasingMode.EaseIn)
+        {
+            // DETERMINE where to start the animation from based on the slide direction
+            Thickness? _initialThickness = null;
+            switch (from)
+            {
+                case SlideInFrom.Left:
+                    _initialThickness = new Thickness(-offset, 0, adjustLayout ? 0 : offset, 0);
+                    break;
+                case SlideInFrom.Top:
+                    _initialThickness = new Thickness(0, -offset, 0, adjustLayout ? 0 : offset);
+                    break;
+                case SlideInFrom.Right:
+                    _initialThickness = new Thickness(adjustLayout ? 0 : offset, 0, -offset, 0);
+                    break;
+                case SlideInFrom.Bottom:
+                    _initialThickness = new Thickness(0, adjustLayout ? 0 : offset, 0, -offset);
+                    break;
+                default:
+                    break;
+            }
+
+            // Init the animation to play
+            var animation = new ThicknessAnimation
+            {
+                From = _initialThickness,
+                To= new Thickness(0),
+                Duration = new Duration(TimeSpan.FromSeconds(duration)),
+                DecelerationRatio = decelerationRatio,
+                EasingFunction = new PowerEase { EasingMode = easing, Power=10 }
+            };
+
+            // Set the target property
+            Storyboard.SetTargetProperty(animation, new PropertyPath("Margin"));
+
+            // Add the animation to the storyboard
+            storyboard.Children.Add(animation);
+        }
+    }
+}
